1f2883298d3a2f7623587068e1897974e89ff892
quang522003 <nguyenhuuquang12a10@gmail.com>
2024-09-18 10:24:46 +0700

    init

---
+++ download-email-csv/src/test/java/org/example/downloademailcsv/DownloadEmailCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.downloademailcsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloadEmailCsvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
